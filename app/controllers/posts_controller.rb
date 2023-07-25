78ecca09d97d00b03fd0b1b9e168c18aaf58639f
taiki yamamoto <ultra0115.20@gmail.com>
2023-07-25 15:54:15 +0900

    createアクションとstrongparameterの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,16 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "新たにWhisperしました！"
+    end
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
