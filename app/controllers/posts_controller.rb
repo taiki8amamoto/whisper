59e4d154ae27d98209fcf278c98ead00bef96d3f
taiki yamamoto <ultra0115.20@gmail.com>
2023-07-25 17:34:51 +0900

    editアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "Whisperを編集しました！"
+    else
+      render :edit
+    end
+  end
+
   private
   
   def post_params
