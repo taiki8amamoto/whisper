57b884d456f06d04887a426d4496c7ad35579a86
taiki yamamoto <ultra0115.20@gmail.com>
2023-07-25 18:21:33 +0900

    confirmアクションの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,10 +10,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: "新たにWhisperしました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "新たにWhisperしました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -38,6 +42,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "Whisperを削除しました！"
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
   end
 
   private
